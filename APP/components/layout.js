001105481a38bedba50c9ac2041770f900676da7
dnzc <danchenzq@gmail.com>
2025-02-06 02:02:05 +0000

    fix favicon

--- APP/components/layout.js
+++ APP/components/layout.js
@@ -6,14 +6,15 @@ export default function Layout({ children }) {
             <Head>
                 <meta name='description' content='Daniel C&apos;s course notes' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <meta name='og:image' content='/favicon.ico' />
                 <meta name='og:title' content='Course Notes | Daniel C' />
                 <meta name='twitter:card' content='summary_large_image' />
+                <meta name="apple-mobile-web-app-title" content="Courses" />
 
-                <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-                <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-                <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-                <link rel='manifest' href='/site.webmanifest' />
+                <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </Head>
             <main>
                 {children}
