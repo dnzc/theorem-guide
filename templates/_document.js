f845bca3883e6ab56a68f63dfd60e369c619db98
dnzc <danchenzq@gmail.com>
2025-06-29 04:18:18 +0100

    try again

--- templates/_document.js
+++ templates/_document.js
@@ -12,10 +12,7 @@ export default function Document() {
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
 
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-title" content="Tripos Guru" />
         <link rel="manifest" href="/site.webmanifest" />
