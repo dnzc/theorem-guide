2573995d9f6011bbc6e17a1f715aa9673e4fac5e
dnzc <danchenzq@gmail.com>
2025-06-29 02:52:26 +0100

    safari mask-icon

--- templates/_document.js
+++ templates/_document.js
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="theme-color" content="#0f0f0f"/> {/* fallback */}
         <Script src="/theme.js" strategy="beforeInteractive"/>
       </Head>
       <body>
