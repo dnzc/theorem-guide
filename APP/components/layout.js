2232550b360c29c17ec672ab3d13badf9b0e2a98
dnzc <danchenzq@gmail.com>
2025-06-29 03:05:08 +0100

    try to add iphone safari tabview favicon

--- APP/components/layout.js
+++ APP/components/layout.js
@@ -12,9 +12,9 @@ export default function Layout({ children }) {
                 <meta property="og:type" content="website" />
                 <meta name="twitter:card" content="summary_large_image" />
 
+                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
                 <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
                 <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-                <link rel="mask-icon" href="/favicon.svg" />
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <meta name="apple-mobile-web-app-title" content="Tripos Guru" />
