144c7b3b6fabc48f0f34f9e597b461dc4a5a416b
dnzc <danchenzq@gmail.com>
2025-06-29 01:00:20 +0100

    og metadata

--- APP/components/layout.js
+++ APP/components/layout.js
@@ -4,11 +4,13 @@ export default function Layout({ children }) {
     return (
         <div>
             <Head>
-                <meta name='description' content='Maths Tripos lecture notes' />
-                <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <meta name='og:title' content='Tripos Guru' />
-                <meta name='twitter:card' content='summary_large_image' />
-                <meta name='apple-mobile-web-app-title' content='Tripos Guru' />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta property="og:site_name" content="Tripos Guru" />
+                <meta property="og:title" content="Tripos Guru" />
+                <meta property="og:description" content="Maths Tripos lecture notes" />
+                <meta property="og:image" itemprop="image" content="<generated>" />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="summary_large_image" />
 
                 <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
                 <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
