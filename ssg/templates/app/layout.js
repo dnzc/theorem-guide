2d81daf74db14883c9ad7c563eb9bc03554db3e9
dnzc <50598509+dnzc@users.noreply.github.com>
2025-07-11 00:39:35 +0100

    update todo

--- ssg/templates/app/layout.js
+++ ssg/templates/app/layout.js
@@ -36,6 +36,10 @@ export const metadata = {
     template: '%s | Tripos Guru', // applies to child routes only
     default: 'Tripos Guru',
   },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#dddddd' },
+    { media: '(prefers-color-scheme: dark)', color: '#1e1e2e' }
+  ],
 }
 
 export default function RootLayout({ children }) {
